f3b9c424f01c86ab57b2277b56e3c723f66cf6f6
Jonghyun Kim <jhkim@dit.ac.kr>
2019-04-14 14:03:01 +0900

    02, 03 예제 소스 시리얼 모니터 설명 페이지 추가

--- 02/02.ino
+++ 02/02.ino
@@ -7,11 +7,18 @@ void setup() {
 }
 
 void loop() {
-  int val = analogRead(A0);
+  // val : 가변저항의 alalog 값
+  int val = analogRead(A0); 
+  
+  Serial.print("가변저항 값: ");
   Serial.println(val);
 
-  // 아날로그 출력을 음정으로 변환
+  // 아날로그 출력을 음(주파수)으로 변환
   int pitch = map(val, 0, 1023, 0, 3000);
   tone(piezoPin, pitch, 50);
+
+  Serial.print("pitch: ");
+  Serial.println(pitch);
+  
   delay(300);
 }
